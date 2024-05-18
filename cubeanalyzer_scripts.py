086d4e67cb2537c811fdcc6227a1cd92ec61fb31
MustaphaEllouze <ellouze.mostafa@gmail.com>
2024-05-18 17:01:56 +0100

    argparse initialization

---
+++ cubeanalyzer_scripts.py
@@ -0,0 +1,38 @@
+import argparse
+
+class ScriptsLauncher:
+
+    @classmethod
+    def parse_args(cls, )->None:
+        parser = argparse.ArgumentParser(
+            prog='Cube Analyzer scripts',
+            description='Script that launches several data analyzing functions for MtG Cubes.',
+        )
+
+        parser.add_argument(
+            '-c', '--cards',
+            help='Add cards from the cards meta-language file in the data file.',
+            action='store_true',
+        )
+
+        parser.add_argument(
+            '-d', '--decks',
+            help='Add decks from the decks meta-language file in the decks file.',
+            action='store_true',
+        )
+
+        parser.add_argument(
+            '-g', '--games',
+            help='Add games from the game meta-language file in the games file.',
+            action='store_true',
+        )
+
+        parser.add_argument(
+            '-C', '--compile',
+            help='Compile the data from the games to compute new elo for cards.',
+            action='store_true',
+        )
+
+        return parser.parse_args()
+
+ScriptsLauncher.parse_args()
